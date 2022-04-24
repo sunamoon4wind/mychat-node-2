46a1095ce809a5229e3fe237e4e47d7192ebe4fc
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-24 17:28:05 +0900

    m

--- server.js
+++ server.js
@@ -62,13 +62,13 @@ server.listen(
 //         }
 //         return false;   // フォーム送信はしない
 //     });
-var btn = document.getElementById('btn');
 
-btn.addEventListener('click', function () {
-    alert(document.myform.mytext.value);
+// var btn = document.getElementById('btn');
+// btn.addEventListener('click', function () {
+//     alert(document.myform.mytext.value);
 
-    //submit()でフォームの内容を送信
-    document.myform.submit();
-})
+//     //submit()でフォームの内容を送信
+//     document.myform.submit();
+// })
 
 
