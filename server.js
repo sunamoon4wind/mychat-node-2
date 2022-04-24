f2d6f8361a7553ff6dac75f52e8fcf2c3c8b0279
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-24 16:30:32 +0900

    server.js 追記１

--- server.js
+++ server.js
@@ -49,16 +49,16 @@ server.listen(
         console.log('Server on port %d', PORT);
     });
 
-// // 「Send」ボタンを押したときの処理
-// $('form').submit(
-//     () => {
-//         console.log('#input_message :', $('#input_message').val());
+// 「Send」ボタンを押したときの処理
+$('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
 
-//         if ($('#input_message').val()) {
-//             // サーバーに、イベント名'new message' で入力テキストを送信
-//             socket.emit('new message', $('#input_message').val());
+        if ($('#input_message').val()) {
+            // サーバーに、イベント名'new message' で入力テキストを送信
+            socket.emit('new message', $('#input_message').val());
 
-//             $('#input_message').val('');    // テキストボックスを空に。
-//         }
-//         return false;   // フォーム送信はしない
-//     });
+            $('#input_message').val('');    // テキストボックスを空に。
+        }
+        return false;   // フォーム送信はしない
+    });
