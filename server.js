7a0cb8e3c9fc44a762bed82c8da05f5aff1c522c
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-24 16:47:45 +0900

    m

--- server.js
+++ server.js
@@ -50,15 +50,15 @@ server.listen(
     });
 
 // 「Send」ボタンを押したときの処理
-// $('form').submit(
-//     () => {
-//         console.log('#input_message :', $('#input_message').val());
+$('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
 
-//         // if ($('#input_message').val()) {
-//         //     // サーバーに、イベント名'new message' で入力テキストを送信
-//         //     socket.emit('new message', $('#input_message').val());
+        // if ($('#input_message').val()) {
+        //     // サーバーに、イベント名'new message' で入力テキストを送信
+        //     socket.emit('new message', $('#input_message').val());
 
-//         //     $('#input_message').val('');    // テキストボックスを空に。
-//         // }
-//         // return false;   // フォーム送信はしない
-//     });
+        //     $('#input_message').val('');    // テキストボックスを空に。
+        // }
+        // return false;   // フォーム送信はしない
+    });
