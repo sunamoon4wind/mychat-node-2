d5d322bf2bf1315328614d60fa3ceb5125cfdde5
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-24 17:41:20 +0900

    m

--- public/client.js
+++ public/client.js
@@ -11,15 +11,35 @@ socket.on(
         console.log('connect');
     });
 
+// 「Send」ボタンを押したときの処理
+$('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
+
+        if ($('#input_message').val()) {
+            // サーバーに、イベント名'new message' で入力テキストを送信
+            socket.emit('new message', $('#input_message').val());
+
+            $('#input_message').val('');    // テキストボックスを空に。
+        }
+        return false;   // フォーム送信はしない
+    });
+
 // サーバーからのメッセージ拡散に対する処理
 // ・サーバー側のメッセージ拡散時の「io.emit( 'spread message', strMessage );」に対する処理
 socket.on(
     'spread message',
-    (strMessage) => {
-        console.log('spread message :', strMessage);
+    //( strMessage ) =>
+    (objMessage) => {
+        //console.log( 'spread message :', strMessage );
+        console.log('spread message :', objMessage);
+
+        // メッセージの整形
+        const strText = objMessage.strDate + ' - ' + objMessage.strMessage;
 
         // 拡散されたメッセージをメッセージリストに追加
-        const li_element = $('<li>').text(strMessage);
+        //const li_element = $( '<li>' ).text( strMessage );
+        const li_element = $('<li>').text(strText);
         $('#message_list').prepend(li_element); // リストの一番上に追加
         //$( '#message_list' ).append( li_element );    // リストの一番下に追加
     });
--- server.js
+++ server.js
@@ -13,6 +13,21 @@ const io = socketIO(server);
 // 定数
 const PORT = process.env.PORT || 1337;
 
+// 関数
+// 数字を２桁の文字列に変換
+const toDoubleDigitString =
+    (num) => {
+        return ("0" + num).slice(-2);   // slice( -2 )で後ろから２文字取り出す。
+    };
+
+// 時刻文字列の作成（書式は「YY/DD/MM hh:mm ss」）
+const makeTimeString =
+    (time) => {
+        return toDoubleDigitString(time.getFullYear()) + '/' + toDoubleDigitString(time.getMonth() + 1) + '/' + toDoubleDigitString(time.getDate())
+            + ' ' + toDoubleDigitString(time.getHours()) + ':' + toDoubleDigitString(time.getMinutes()) + ' ' + toDoubleDigitString(time.getSeconds());
+    }
+
+
 // 接続時の処理
 // ・サーバーとクライアントの接続が確立すると、
 // 　サーバー側で、'connection'イベント
@@ -21,6 +36,7 @@ io.on(
     'connection',
     (socket) => {
         console.log('connection');
+
         // 切断時の処理
         // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
         socket.on(
@@ -28,15 +44,26 @@ io.on(
             () => {
                 console.log('disconnect');
             });
+
         // 新しいメッセージ受信時の処理
         // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
         socket.on(
             'new message',
             (strMessage) => {
                 console.log('new message', strMessage);
-                // 送信元含む全員に送信
-                io.emit('spread message', strMessage);
 
+                // 現在時刻の文字列の作成
+                const strNow = makeTimeString(new Date());
+
+                // メッセージオブジェクトの作成
+                const objMessage = {
+                    strMessage: strMessage,
+                    strDate: strNow
+                }
+
+                // 送信元含む全員に送信
+                //io.emit( 'spread message', strMessage );
+                io.emit('spread message', objMessage);
             });
     });
 
