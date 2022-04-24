8ba9dd4c0343d1286b0d76125a044c95e35b633f
sunamoon4wind <sunamoon4wind@gmail.com>
2022-04-24 15:43:13 +0900

    test2

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ io.on(
     'connection',
     (socket) => {
         console.log('connection');
+        // 切断時の処理
+        // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
+        socket.on(
+            'disconnect',
+            () => {
+                console.log('disconnect');
+            });
+        // 新しいメッセージ受信時の処理
+        // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
+        socket.on(
+            'new message',
+            (strMessage) => {
+                console.log('new message', strMessage);
+            });
+
     });
 
 
@@ -33,4 +48,18 @@ server.listen(
     PORT,
     () => {
         console.log('Server on port %d', PORT);
+    });
+
+// 「Send」ボタンを押したときの処理
+$('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
+
+        if ($('#input_message').val()) {
+            // サーバーに、イベント名'new message' で入力テキストを送信
+            socket.emit('new message', $('#input_message').val());
+
+            $('#input_message').val('');    // テキストボックスを空に。
+        }
+        return false;   // フォーム送信はしない
     });
